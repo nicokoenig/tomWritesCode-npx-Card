5267fe47e776ade15920a1add13547094ba01e98
Thomas Whitaker <tom@tomwritescode.com>
2018-12-20 13:01:02 +1100

    Initial Commit
    npx card from Tierney Cyren (@bitandbang)
    
    https://github.com/bnb/bitandbang

---
+++ README.md
@@ -0,0 +1,10 @@
+
+Big thanks to Tierney Cyren ( @bitandbang ) for posting the codebase on GitHub so all of the internet can now have these. 
+
+# Usage
+Via npx:
+```
+npx tomwritescode
+```
+
+Originally made by someone much smarter than I. 
---
+++ bin/card.js
@@ -0,0 +1,62 @@
+#!/usr/bin/env node
+// 👆 Used to tell Node.js that this is a CLI tool
+
+// Pull in our modules
+var chalk = require("chalk");
+var boxen = require("boxen");
+
+// Define options for Boxen
+let options = {
+	padding: 1,
+	margin: 1,
+	borderStyle: "round"
+};
+// Text + chalk definitions
+let data = {
+	name: chalk.white("Thomas Whitaker /"),
+	handle: chalk.cyan("tomWritesCode"),
+	work: chalk.white("Developer / Caffeine addict / Serial Ranter"),
+	twitter: chalk.cyan("https://twitter.com/tomWritesCode"),
+  github: chalk.cyan("https://github.com/tomWritesCode"),
+	linkedin: chalk.cyan(
+		"https://www.linkedin.com/in/thomas-whitaker-b961b9112/"
+	),
+	web: chalk.cyan("https://tomwritescode.com"),
+	npx: chalk.white("npx tomwritescode"),
+	labelWork: chalk.white.bold("      Work:"),
+	labelTwitter: chalk.white.bold("   Twitter:"),
+	labelGitHub: chalk.white.bold("    GitHub:"),
+	labelLinkedIn: chalk.white.bold("  LinkedIn:"),
+	labelWeb: chalk.white.bold("       Web:"),
+	labelCard: chalk.white.bold("      Card:")
+};
+
+// Actual strings we're going to output
+var newline = "\n";
+var heading = `${data.name} ${data.handle}`;
+var working = `${data.labelWork}  ${data.work}`;
+var twittering = `${data.labelTwitter}  ${data.twitter}`;
+var githubing = `${data.labelGitHub}  ${data.github}`;
+var linkedining = `${data.labelLinkedIn}  ${data.linkedin}`;
+var webing = `${data.labelWeb}  ${data.web}`;
+var carding = `${data.labelCard}  ${data.npx}`;
+
+// Put all our output together into a single variable so we can use boxen effectively
+let output =
+	heading +
+	newline +
+	newline +
+	working +
+	newline +
+	twittering +
+	newline +
+	githubing +
+	newline +
+	linkedining +
+	newline +
+	webing +
+	newline +
+	newline +
+	carding;
+
+console.log(chalk.green(boxen(output, options)));
